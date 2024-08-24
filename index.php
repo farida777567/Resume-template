d530946f0cd1547c231972d019a697a95902b0d3
farida777567 <142094751+farida777567@users.noreply.github.com>
2024-08-24 13:33:08 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+include 'personalData.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8"/>
+    <title></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
+
+    
+    <link href="http://fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css"/>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+
+   
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="vendor/swiper/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+
+    
+    <link href="css/layout.min.css" rel="stylesheet" type="text/css"/>
+
+   
+    <link rel="shortcut icon" href="favicon.ico"/>
+</head>
+<body id="body" data-spy="scroll" data-target=".header">
+    <?php include 'header.php'; ?>
+    <?php include 'slider.php'; ?>
+    <?php include 'about.php'; ?>
+    <?php include 'experience.php'; ?>
+    <?php include 'work.php'; ?>
+    <?php include 'contact.php'; ?>
+    <?php include 'footer.php'; ?>
+
+    
+    <script src="vendor/jquery.min.js" type="text/javascript"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="vendor/jquery.easing.js" type="text/javascript"></script>
+    <script src="vendor/jquery.back-to-top.js" type="text/javascript"></script>
+    <script src="vendor/jquery.smooth-scroll.js" type="text/javascript"></script>
+    <script src="vendor/jquery.wow.min.js" type="text/javascript"></script>
+    <script src="vendor/jquery.parallax.min.js" type="text/javascript"></script>
+    <script src="vendor/swiper/js/swiper.jquery.min.js" type="text/javascript"></script>
+    <script src="js/layout.min.js" type="text/javascript"></script>
+</body>
+</html>
